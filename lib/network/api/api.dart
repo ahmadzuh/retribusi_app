af907ec344de734832e9b09cad5317df18653993
ahmadzuh <ahmad65u@gmail.com>
2020-09-08 08:42:32 +0700

    Change Service

--- lib/network/api/api.dart
+++ lib/network/api/api.dart
@@ -2,5 +2,6 @@ class ApiService {
   static const String baseUrl = 'https://retribusi.jambikota.go.id/api/v1';
   static const String loginUrl =
       'https://retribusi.jambikota.go.id/api/v1/login';
-  static const String listUrl = 'https://retribusi.jambikota.go.id/api/v1/me';
+  static const String listUrl =
+      'https://retribusi.jambikota.go.id/api/v1/area-tagih';
 }
